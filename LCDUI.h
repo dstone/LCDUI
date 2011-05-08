1c68080ae9b49c62ed1bc551d92819a40d515d66
dstone <thedanstone@gmail.com>
2011-05-08 17:02:42 -0400

    add a little doc

--- LCDUI.h
+++ LCDUI.h
@@ -23,12 +23,31 @@ class LCDUI {
          * \param col    Starting column to display value.
          * \param row    Row to display value.
          * \param wrap   Boolean value. If true, values will wrap between 0 and maxVal.
-         *               If false, no wrapping.
+         *               If false, no wrapping. Default is true (wrapping enabled).
          */
         int getInt( char* format, int maxVal, int col, int row, int wrap=true );
 
+        /**
+         * Displays a single-screen "menu"--using the nav controls causes the 
+         * cursor to move to the next in a set of column/row points on the 
+         * screen. By activating the action button, a point is selected and the
+         * method returns the index of the selected point.
+         * \param locs  An array of column/row locations on the LCD.
+         * \param count The size of the locs array.
+         * \param pos   The index of a location in the locs array to be used as 
+         *              the initial location of the cursor.
+         */
         int cursorMenu( int locs[][2], int count, int pos=0 );
 
+        /**
+         * Displays a vertical-scrolling menu. Selection is indicated by a 
+         * right arrow. Nav controls are used to move the arrow up and down.
+         * The action control causes the method to return the index of the
+         * currently-selected menu item.
+         * \param menuItems An array of char* representing the menu contents to
+         *                  be displayed.
+         * \param menuLen   The length of the menuItems array.
+         */
         int stringMenu( char *menuItems[], size_t menuLen );
 
         int getTime();
